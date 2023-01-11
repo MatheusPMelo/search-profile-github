347cec87af7ecfe88d8f7ce5659277883b3e23ed
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-01-11 01:44:57 -0300

    mobile e tablet

--- src/components/repositories/style.ts
+++ src/components/repositories/style.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 export const ContainerRepositories = styled.section`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+    gap: 1rem;
     margin-top: 4rem;
+    width: 100%;
+    justify-content: center;
 
     .repository{
         background: ${props => props.theme.colors.secondary};
