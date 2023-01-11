1fe1901f3f9e9ad93cfeb0c7d580ef0fbcf32619
Matheus Melo <matheusprestesdmelo744@gmail.com>
2023-01-11 03:07:55 -0300

    removendo FC sem  do import

--- src/components/repositories/index.tsx
+++ src/components/repositories/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { useState } from 'react';
 import { ContainerRepositories, MainContainerRepositories } from './style'
 
 const Repositories: React.FC<any> = ({
